d49d6960c1feca589c7de15cc36a458dada0a662
hanzh0816 <15091693112@163.com>
2024-06-03 17:46:13 +0800

    feat(data pipeline): :sparkles: implement load .npy files to input

--- mmdet/datasets/transforms/formatting.py
+++ mmdet/datasets/transforms/formatting.py
@@ -66,8 +66,8 @@ class PackDetInputs(BaseTransform):
                 sample.
         """
         packed_results = dict()
-        if 'img' in results:
-            img = results['img']
+        if "img" in results:
+            img = results["img"]
             if len(img.shape) < 3:
                 img = np.expand_dims(img, -1)
             # To improve the computational speed by by 3-5 times, apply:
@@ -77,17 +77,29 @@ class PackDetInputs(BaseTransform):
             # `torch.permute()` followed by `torch.contiguous()`
             # Refer to https://github.com/open-mmlab/mmdetection/pull/9533
             # for more details
-            if not img.flags.c_contiguous:
-                img = np.ascontiguousarray(img.transpose(2, 0, 1))
-                img = to_tensor(img)
+            if "event" in results:
+                # event-based image shape is T*C*H*W
+                T, C, H, W = img.shape
+                img = img.reshape(
+                    T * C, H, W
+                )  # reshape as T*C,H,W to organize into batches
+                if not img.flags.c_contiguous:
+                    img = np.ascontiguousarray(img)
+                    img = to_tensor(img)
+                else:
+                    img = to_tensor(img).contiguous()
             else:
-                img = to_tensor(img).permute(2, 0, 1).contiguous()
+                if not img.flags.c_contiguous:
+                    img = np.ascontiguousarray(img.transpose(2, 0, 1))
+                    img = to_tensor(img)
+                else:
+                    img = to_tensor(img).permute(2, 0, 1).contiguous()
 
-            packed_results['inputs'] = img
+            packed_results["inputs"] = img
 
-        if 'gt_ignore_flags' in results:
-            valid_idx = np.where(results['gt_ignore_flags'] == 0)[0]
-            ignore_idx = np.where(results['gt_ignore_flags'] == 1)[0]
+        if "gt_ignore_flags" in results:
+            valid_idx = np.where(results["gt_ignore_flags"] == 0)[0]
+            ignore_idx = np.where(results["gt_ignore_flags"] == 1)[0]
 
         data_sample = DetDataSample()
         instance_data = InstanceData()
@@ -96,38 +108,41 @@ class PackDetInputs(BaseTransform):
         for key in self.mapping_table.keys():
             if key not in results:
                 continue
-            if key == 'gt_masks' or isinstance(results[key], BaseBoxes):
-                if 'gt_ignore_flags' in results:
-                    instance_data[
-                        self.mapping_table[key]] = results[key][valid_idx]
-                    ignore_instance_data[
-                        self.mapping_table[key]] = results[key][ignore_idx]
+            if key == "gt_masks" or isinstance(results[key], BaseBoxes):
+                if "gt_ignore_flags" in results:
+                    instance_data[self.mapping_table[key]] = results[key][valid_idx]
+                    ignore_instance_data[self.mapping_table[key]] = results[key][
+                        ignore_idx
+                    ]
                 else:
                     instance_data[self.mapping_table[key]] = results[key]
             else:
-                if 'gt_ignore_flags' in results:
+                if "gt_ignore_flags" in results:
                     instance_data[self.mapping_table[key]] = to_tensor(
-                        results[key][valid_idx])
+                        results[key][valid_idx]
+                    )
                     ignore_instance_data[self.mapping_table[key]] = to_tensor(
-                        results[key][ignore_idx])
+                        results[key][ignore_idx]
+                    )
                 else:
-                    instance_data[self.mapping_table[key]] = to_tensor(
-                        results[key])
+                    instance_data[self.mapping_table[key]] = to_tensor(results[key])
         data_sample.gt_instances = instance_data
         data_sample.ignored_instances = ignore_instance_data
 
-        if 'proposals' in results:
+        if "proposals" in results:
             proposals = InstanceData(
-                bboxes=to_tensor(results['proposals']),
-                scores=to_tensor(results['proposals_scores']))
+                bboxes=to_tensor(results["proposals"]),
+                scores=to_tensor(results["proposals_scores"]),
+            )
             data_sample.proposals = proposals
 
-        if 'gt_seg_map' in results:
+        if "gt_seg_map" in results:
             gt_sem_seg_data = dict(
-                sem_seg=to_tensor(results['gt_seg_map'][None, ...].copy()))
+                sem_seg=to_tensor(results["gt_seg_map"][None, ...].copy())
+            )
             gt_sem_seg_data = PixelData(**gt_sem_seg_data)
-            if 'ignore_index' in results:
-                metainfo = dict(ignore_index=results['ignore_index'])
+            if "ignore_index" in results:
+                metainfo = dict(ignore_index=results["ignore_index"])
                 gt_sem_seg_data.set_metainfo(metainfo)
             data_sample.gt_sem_seg = gt_sem_seg_data
 
@@ -136,13 +151,13 @@ class PackDetInputs(BaseTransform):
             if key in results:
                 img_meta[key] = results[key]
         data_sample.set_metainfo(img_meta)
-        packed_results['data_samples'] = data_sample
+        packed_results["data_samples"] = data_sample
 
         return packed_results
 
     def __repr__(self) -> str:
         repr_str = self.__class__.__name__
-        repr_str += f'(meta_keys={self.meta_keys})'
+        repr_str += f"(meta_keys={self.meta_keys})"
         return repr_str
 
 
